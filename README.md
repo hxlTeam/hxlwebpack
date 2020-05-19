d181642eec6b4949243a78f25c43de89661d53f4
huangxueliang <huangxueliang@lbdfun.com>
2020-05-19 14:05:40 +0800

    feat:实现replaceLoader

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hxlwebpack
+
+# 学习webpack打包原理
+
+# 编写一个自己的loader
+# 编写一个自己的plugin
+# 手写一个bundle.js
---
+++ loaders/replaceLoader.js
@@ -0,0 +1,13 @@
+module.exports = function (source) {
+  // console.log(this.query);
+  // return source.replace('world', '黄学良:' + this.query.name);
+
+  // const code = source.replace('world', '黄学良:' + this.query.name);
+  // this.callback(null, code);
+
+  const callback = this.async();
+  setTimeout(() => {
+    const code = source.replace('world', '黄学良:' + this.query.name);
+    callback(null,code);
+  }, 2000);
+}
